9ed390600e446d6aba414d62a1eedca3ffe233e4
cedzo2 <89861256+cedzo2@users.noreply.github.com>
2021-12-07 14:33:47 -0500

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,14 @@
+// popup.js
+document.addEventListener('DOMContentLoaded', function () {
+    var checkButton = document.getElementById('searchbutton');
+    checkButton.addEventListener('click', function () {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { method: "changePage" }, function (response) {
+                if (response.method == "changePage") {
+                    links = JSON.parse(response.text);
+                }
+                alert(links.join('\r\n'))
+            });
+        });
+    }, false);
+}, false);
