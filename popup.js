2c6ebc4c7828e0f0b2538af34cf7205621d4e3fe
Ellie Zhu <ellie.zhu@nytimes.com>
2021-12-07 20:06:52 -0500

    add scraping:

--- popup.js
+++ popup.js
@@ -8,6 +8,52 @@ document.addEventListener('DOMContentLoaded', function () {
                     links = JSON.parse(response.text);
                 }
                 alert(links.join('\r\n'))
+
+                scrape(links)
+    
+                async function scrape(array) {
+                    let outputs = [];
+                    await asyncForEach(array, async (u) => {
+                        if(u.startsWith("https://en.m.wikipedia.org/wiki/") && !u.endsWith(".jpg")){
+                            outputs.push(await generate(u))
+                        }
+                });
+                    // results here
+                    // console.log(outputs) 
+                    // alert(outputs.join('\r\n'))
+                    // alert("Ellie update")
+                }
+                
+
+                async function asyncForEach(array, callback) {
+                for (let index = 0; index < array.length; index++) {
+                  await callback(array[index], index, array)
+                }
+                }
+
+                async function generate(url) {
+                    const result = await open_url(url);
+                    return result;
+                }
+
+                async function open_url(url) {
+                    alert(url)
+                    const response = await fetch(url, {
+                        mode: "no-cors"
+                    }).catch((response_error) => console.error(`Error:`, response_error)
+                  )
+                  if (response.ok) {
+                    const text = await response.text()
+                    const parser = new DOMParser()
+                    const doc = parser.parseFromString(text, `text/html`)
+                    let body = doc.querySelector(`.mw-parser-output`).innerHTML
+                    body = body.replace(/<style.*?<\/style>/g, '')
+                    const result = body.replace(/(<([^>]+)>)/gi, "").replace(/(^[ \t]*\n)/gm, "")
+                    return result
+                  } else {
+                    return `Error. Contact Ellie for debugging`;
+                  }
+                }
             });
         });
     }, false);
